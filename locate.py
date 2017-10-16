de681a5c1747f3bd4345a60c0c6bee1e5ce62709
pabloSRI <pcumpian1@gmail.com>
2017-10-17 01:12:10 +0200

    added a param option location

--- locate.py
+++ locate.py
@@ -1,6 +1,16 @@
 import os
+import sys
 import time
 import datetime
+
+option = str(sys.argv[1])
+
+try:
+	option
+except Null:
+	print ""	
+else:
+	print "cambiando a configuracion de " + option
 
 ruta="/etc/network/"
 
@@ -8,12 +18,12 @@ now = datetime.datetime.now()
 
 x = now.hour
 
-if x >= 8 and x < 15:
+if (x >= 8 and x < 15) or (option == "clase") :
 	print "estas en clase"
 
 	#cambiamos el nombre del archivo clase por interfaces solo
 	os.system("cat " + ruta + "interfaces_clase > " + ruta + "interfaces")
-elif x >= 15 or x < 8:
+elif (x >= 15 or x < 8) or (option == "casa"):
 	print "estas en casa"
 
 	#cambiamos el nombre del archivo casa por interfaces solo
